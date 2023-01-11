ee74f5bf785ab59000132854628ac4ef1da323b2
andrey <andrey.kozhevnikov@devexpress.com>
2023-01-11 16:45:30 +0400

    company

---
+++ EFCoreCustomLogonAll.Module/BusinessObjects/Company.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.EF;
+
+namespace EFCustomLogon.Module.BusinessObjects;
+
+[DefaultClassOptions]
+public class Company : BaseObject {
+    public virtual string Name { get; set; }
+    public virtual IList<ApplicationUser> ApplicationUsers { get; set; } = new ObservableCollection<ApplicationUser>();
+}
