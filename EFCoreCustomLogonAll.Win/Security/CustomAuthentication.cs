bce091aa02d52274121a1ae17e1b75fd6b4d4adf
andrey <andrey.kozhevnikov@devexpress.com>
2023-01-11 16:51:38 +0400

    security classes win

---
+++ EFCoreCustomLogonAll.Win/Security/CustomAuthentication.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base.Security;
+using EFCustomLogonWin.Module.BusinessObjects;
+
+namespace EFCustomLogon.Module.BusinessObjects;
+
+public class CustomAuthentication : AuthenticationBase, IAuthenticationStandard {
+    private CustomLogonParameters customLogonParameters;
+    public CustomAuthentication() {
+        customLogonParameters = new CustomLogonParameters();
+    }
+    public override void Logoff() {
+        base.Logoff();
+        customLogonParameters = new CustomLogonParameters();
+    }
+    public override void ClearSecuredLogonParameters() {
+        customLogonParameters.Password = "";
+        base.ClearSecuredLogonParameters();
+    }
+    public override object Authenticate(IObjectSpace objectSpace) {
+
+        ApplicationUser applicationUser = objectSpace.FirstOrDefault<ApplicationUser>(e => e.UserName == customLogonParameters.UserName);
+
+        if(applicationUser == null)
+            throw new ArgumentNullException("Employee");
+
+        if(!((IAuthenticationStandardUser)applicationUser).ComparePassword(customLogonParameters.Password))
+            throw new AuthenticationException(
+                applicationUser.UserName, "Password mismatch.");
+
+        return applicationUser;
+    }
+
+    public override void SetLogonParameters(object logonParameters) {
+        this.customLogonParameters = (CustomLogonParameters)logonParameters;
+    }
+
+    public override IList<Type> GetBusinessClasses() {
+        return new Type[] { typeof(CustomLogonParameters) };
+    }
+    public override bool AskLogonParametersViaUI {
+        get { return true; }
+    }
+    public override object LogonParameters {
+        get { return customLogonParameters; }
+    }
+    public override bool IsLogoffEnabled {
+        get { return true; }
+    }
+}
